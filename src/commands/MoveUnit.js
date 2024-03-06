0b2805ea4bdb3587d025a53e3e23110b97fa0ad5
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-03-06 15:30:34 -0600

    clamped to world boundaries

--- src/commands/MoveUnit.js
+++ src/commands/MoveUnit.js
@@ -1,26 +1,36 @@
+import gameParams from "../../config/gameParams.json";
+
 export class MoveUnit {
   constructor(entity) {
     this.entity = entity;
+    this.boundaries = gameParams.world.boundaries; // Access boundaries from gameParams
   }
 
   up(distance) {
-    this.entity.lastY = this.entity.position.y;
-    this.entity.position.y -= distance;
+    const newY = this.entity.position.y - distance;
+    this.clampY(newY); // Ensure newY stays within boundaries
   }
 
   down(distance) {
-    this.entity.lastY = this.entity.position.y;
-    this.entity.position.y += distance;
+    const newY = this.entity.position.y + distance;
+    this.clampY(newY); // Ensure newY stays within boundaries
   }
 
   left(distance) {
-    this.entity.lastX = this.entity.position.x;
-    this.entity.position.x -= distance;
-  }
-  right(distance) {
-    this.entity.lastX = this.entity.position.x;
-    this.entity.position.x += distance;
+    const newX = this.entity.position.x - distance;
+    this.clampX(newX); // Ensure newX stays within boundaries
   }
 
-  // Optional: Add validation/bounds checking or error handling within methods
+  right(distance) {
+    const newX = this.entity.position.x + distance;
+    this.clampX(newX); // Ensure newX stays within boundaries
+  }
+
+  clampX(newX) {
+    this.entity.position.x = Math.max(this.boundaries.minX, Math.min(newX, this.boundaries.maxX - this.entity.width));
+  }
+
+  clampY(newY) {
+    this.entity.position.y = Math.max(this.boundaries.minY, Math.min(newY, this.boundaries.maxY - this.entity.height));
+  }
 }
